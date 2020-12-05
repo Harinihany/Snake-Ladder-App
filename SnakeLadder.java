4a47470b08914475b341eeb7f43c33a49f82b9e5
Harinihany <harinihany03@gmail.com>
2020-12-05 10:58:10 +0530

    refactor: variable for player

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,9 +2,9 @@
 public class SnakeLadder {
 
 	static int getInitPosition() {
-		
-		int pos = 0;
-		return pos =0;   
+		int player = 0;
+		return player;
+	   
 	}
 	public static void main(String[] args) {
 		System.out.println("get position"+getInitPosition());
