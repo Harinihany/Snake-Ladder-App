f1364b95db928bec9c9b0db5ce9b76655512b2dc
Harinihany <harinihany03@gmail.com>
2020-12-05 10:23:59 +0530

    added: code for initialising position

---
+++ SnakeLadder.java
@@ -0,0 +1,14 @@
+
+public class SnakeLadder {
+
+	static int getInitPosition() {
+		
+		int pos = 0;
+		return pos =0;   
+	}
+	public static void main(String[] args) {
+		System.out.println("get position"+getInitPosition());
+
+	}
+
+}
