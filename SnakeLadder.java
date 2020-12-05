d4b6b96ced923309f99cba6facb56c1b0f6aae7b
Harinihany <harinihany03@gmail.com>
2020-12-05 11:43:31 +0530

    added: welcome message

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,14 +1,14 @@
 
 public class SnakeLadder {
-
+	//Initializing variables;
 	static int getInitPosition() {
 		int player = 0;
 		return player;
-	   
-	}
+	   }
+	
+	
 	public static void main(String[] args) {
-		System.out.println("get position"+getInitPosition());
-
+		System.out.println("welcome to snake and ladder");
 	}
 
 }
