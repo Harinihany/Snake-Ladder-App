887401875d8501f1dca0c8b038c008ac43a09157
Harinihany <harinihany03@gmail.com>
2020-12-05 12:12:45 +0530

    added: code for random

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,14 +1,20 @@
+import java.util.Random;
 
 public class SnakeLadder {
 	//Initializing variables;
-	static int getInitPosition() {
-		int player = 0;
-		return player;
-	   }
-	
-	
-	public static void main(String[] args) {
-		System.out.println("welcome to snake and ladder");
-	}
-
+		static int player = 0;
+		
+		public static void main(String[] args) {
+			System.out.println("Number after rolling dice"+rollDice());
+		}
+			//UC-2-Generating Random Number;
+		static  int rollDice() {
+		    int n = 0;
+		    Random r = new Random();
+		    n=r.nextInt(7);
+		    return n;
+		} 
+	   
 }
+
+
